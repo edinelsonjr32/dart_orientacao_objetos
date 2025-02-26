9181867bbe6f12a023c279070acc47e7edf27675
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-02-25 23:47:18 -0300

    Criando objetos, acessando valores e guardando contas

--- bin/bank.dart
+++ bin/bank.dart
@@ -3,16 +3,23 @@ import 'dart:ffi';
 import 'package:bank/bank.dart' as bank;
 
 void main() {
-  
   Conta contaMatheus = Conta('Edinelson', 1000);
   Conta contaRoberta = Conta('Roberta', 2000);
-  print("O saldo da conta da ${contaRoberta.titular} é: R\$:${contaRoberta.saldo}");
+  print(
+      "O saldo da conta da ${contaRoberta.titular} é: R\$:${contaRoberta.saldo}");
   contaRoberta.saldo = 333.09;
-  print("O saldo da conta da ${contaRoberta.titular} é: R\$:${contaRoberta.saldo}");
+  print(
+      "O saldo da conta da ${contaRoberta.titular} é: R\$:${contaRoberta.saldo}");
+
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+
+  for (Conta conta in contas){
+    print("Titular: ${conta.titular}, saldo ${conta.saldo}");
+  }
 }
 
 class Conta {
   String titular;
-  double saldo ;
+  double saldo;
   Conta(this.titular, this.saldo);
 }
