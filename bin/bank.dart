37dee7f812deee64ba1a7e485f29119c7c71a9f5
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-02-26 00:23:00 -0300

    Imprimindo extrato e acessando propriedades privadas

--- bin/bank.dart
+++ bin/bank.dart
@@ -4,25 +4,23 @@ import 'package:bank/conta.dart';
 void main() {
   Conta contaMatheus = Conta('Edinelson', 1000);
   Conta contaRoberta = Conta('Roberta', 2000);
-  print(
-      "O saldo da conta da ${contaRoberta._titular} é: R\$:${contaRoberta._saldo}");
-  contaRoberta._saldo = 333.09;
-  print(
-      "O saldo da conta da ${contaRoberta._titular} é: R\$:${contaRoberta._saldo}");
+  contaRoberta.imprimirSaldo();
+  contaRoberta.receberSaldo(333.09);
+  contaRoberta.imprimirSaldo();
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
   for (Conta conta in contas) {
-    print("Titular: ${conta._titular}, saldo ${conta._saldo}");
+    conta.imprimirSaldo();
   }
 
   contaRoberta.deposito(1500);
 
-  print("Titular: ${contaRoberta._titular}, saldo ${contaRoberta._saldo}");
+  contaRoberta.imprimirSaldo();
 
   contaRoberta.enviar(contaMatheus, 400);
-
-  print("Titular: ${contaRoberta._titular}, saldo ${contaRoberta._saldo}");
-  print("Titular: ${contaMatheus._titular}, saldo ${contaMatheus._saldo}");
+  contaRoberta.imprimirSaldo();
+  contaMatheus.imprimirSaldo();
+  
 }
 
--- lib/conta.dart
+++ lib/conta.dart
@@ -12,7 +12,19 @@ class Conta {
     if (this._saldo >= valor) {
       this._saldo -= valor;
       conta._saldo += valor;
-      print("Novo saldo do(a) ${this._titular} é: ${this._saldo}");
+      print("Foi enviado ${valor} de ${this._titular} para ${conta._titular}");
     }
+  }
+
+  void imprimirSaldo(){
+    print('O saldo do ${this._titular} é: ${this._saldo}');
+  }
+
+  void  receberSaldo(double valor){
+     this._saldo += valor;
+  }
+
+  void imprimirTitular(){
+    print('A conta é do(a) ${this._titular}');
   }
 }
