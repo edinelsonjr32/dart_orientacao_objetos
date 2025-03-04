41ec972ba696786117acfec6b580a03a5ec2bf06
Edinelson Luis de sousa junior <edinelsonjr.ufopa@gmail.com>
2025-03-04 16:16:52 -0300

    Trabalhando com Mixin em Dart

--- lib/conta.dart
+++ lib/conta.dart
@@ -45,9 +45,55 @@ class ContaCorrente extends Conta {
 
   @override
   void enviar(Conta conta, double valor) {
-    if (_saldo + emprestimo >= valor){
+    if (_saldo + emprestimo >= valor) {
       _saldo -= valor;
       imprimirSaldo();
     }
+  }
+}
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double valorTaxado(double valor) {
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super._titular, super._saldo);
+
+  @override
+  void enviar(Conta conta, double valor) {
+    // TODO: implement enviar
+    if (_saldo >= valorTaxado(valor)) {
+      _saldo -= valor + valorTaxado(valor);
+      imprimirSaldo();
+    }
+  }
+
+  @override
+  void deposito(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimirSaldo();
+  }
+}
+
+
+class ContaInvestimento extends Conta with Imposto{
+  ContaInvestimento(super._titular, super._saldo);
+
+  @override
+  void enviar(Conta conta, double valor) {
+    if (_saldo >= valorTaxado(valor)) {
+      _saldo -= valor + valorTaxado(valor);
+      imprimirSaldo();
+    }
+  }
+
+  @override
+  void deposito(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimirSaldo();
   }
 }
